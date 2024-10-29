5aa250a2b546b40be8f1c0aae4cbc675a8fac83f
LIN Loris <loris.lin@epita.fr>
2024-10-29 16:15:58 +0100

    feat: pong.py

---
+++ pong.py
@@ -0,0 +1,13 @@
+import gymnasium as gym
+import ale_py
+
+gym.register_envs(ale_py)
+
+env = gym.make("ALE/Pong-v5", render_mode="human")
+obs, info = env.reset()
+episode_over =  False
+while not episode_over:
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(action)
+    episode_over = terminated or truncated
+env.close()
